852810610863e9149ef5fc0c9b726d8c0309f9df
konczdam98 <konczdam98@gmail.com>
2023-06-08 10:41:25 -0700

    Add backend (Spring initializer)

---
+++ forma1-teams-application/src/main/java/hu/konczdam/forma1/Forma1TeamsApplication.java
@@ -0,0 +1,13 @@
+package hu.konczdam.forma1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Forma1TeamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Forma1TeamsApplication.class, args);
+    }
+
+}
---
+++ forma1-teams-application/src/test/java/hu/konczdam/forma1/Forma1TeamsApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.konczdam.forma1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Forma1TeamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
