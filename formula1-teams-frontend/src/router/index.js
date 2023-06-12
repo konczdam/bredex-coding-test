a037273bb2ff509d65cfb8cb09c341bf0fbad466
konczdam98 <konczdam98@gmail.com>
2023-06-12 01:06:52 -0700

    Add basic frontend based on PrimeVue and Sakai theme

---
+++ formula1-teams-frontend/src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import AppLayout from '@/layout/AppLayout.vue';
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            component: AppLayout,
+            children: [
+                {
+                    path: '/',
+                    name: 'dashboard',
+                    component: () => import('@/views/pages/Landing.vue')
+                }
+            ]
+        },
+        {
+            path: '/auth/login',
+            name: 'login',
+            component: () => import('@/views/pages/auth/Login.vue')
+        },
+        {
+            path: '/auth/access',
+            name: 'accessDenied',
+            component: () => import('@/views/pages/auth/Access.vue')
+        },
+        {
+            path: '/auth/error',
+            name: 'error',
+            component: () => import('@/views/pages/auth/Error.vue')
+        }
+    ]
+});
+
+export default router;
---
+++ formula1-teams-frontend/src/views/pages/Landing.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useLayout } from '@/layout/composables/layout';
+import { computed } from 'vue';
+import Crud from '@/views/pages/Crud.vue';
+
+const { contextPath } = useLayout();
+
+const logoUrl = computed(() => {
+    return `${contextPath}layout/images/car-of-formula-1.svg`;
+});
+</script>
+
+<template>
+    <div class="surface-0 flex justify-content-center">
+        <div id="home" class="landing-wrapper overflow-hidden">
+            <div class="py-4 px-4 mx-0 md:mx-6 lg:mx-8 lg:px-8 flex align-items-center justify-content-between relative lg:static mb-3">
+                <a class="flex align-items-center" href="#"> <img :src="logoUrl" alt="Sakai Logo" height="50" class="mr-0 lg:mr-2" /><span class="text-900 font-medium text-2xl line-height-3 mr-8">Formula-1</span> </a>
+                <a class="cursor-pointer block lg:hidden text-700 p-ripple" v-ripple v-styleclass="{ selector: '@next', enterClass: 'hidden', leaveToClass: 'hidden', hideOnOutsideClick: true }">
+                    <i class="pi pi-bars text-4xl"></i>
+                </a>
+                <div class="align-items-center surface-0 flex-grow-1 justify-content-end hidden lg:flex absolute lg:static w-full left-0 px-6 lg:px-0 z-2" style="top: 120px">
+                    <div class="flex justify-content-between lg:block border-top-1 lg:border-top-none surface-border py-3 lg:py-0 mt-3 lg:mt-0">
+                        <Button label="Login" class="p-button-rounded border-none ml-5 font-light text-white line-height-2 bg-blue-500"></Button>
+                    </div>
+                </div>
+            </div>
+
+            <Crud />
+        </div>
+    </div>
+</template>
